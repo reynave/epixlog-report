8c9142b157559056eb85982bb56945beb85ff085
felix <felix.renaidl@gmaio.com>
2024-10-01 18:21:56 +0700

    update add ejs

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 const port = process.env.PORT;
 const connectionString = "Driver={"+process.env.SQL_DRIVE+"};Server="+process.env.SQL_SERVER+";Database="+process.env.SQL_DATABASE+";Trusted_Connection=yes;";
 
+
 //Solve CORS Origin issue
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -17,33 +18,30 @@ app.use((req, res, next) => {
     next();
   });
   
-
-// sql.query(connectionString, "select GETDATE() as 'today'", (err, rows) => {
-//     if (err) {
-//         console.error('Error: ' + err);
-//     } else {
-//         console.log('DB Success',rows);
-//     }
-// });
+app.set('view engine', 'ejs'); 
 
 
 app.use(express.json());
-app.use(express.static(__dirname + '/public'));
 
 app.use('/report', require('./controllers/report'));
 
-app.get('/', (req, res) => {
-    fs.readFile(__dirname + '/public/index.html', 'utf8', (err, text) => {
-        res.send(text);
-    });
+app.get('/', (req, res) => { 
+    var pjson = require('./package.json');
+    console.log('masuk',pjson.version);
+    var locals = { 
+        ver: pjson.version
+      };
+    res.render('index',locals);
 });
 
 app.get('/table.html', (req, res) => {
-    fs.readFile(__dirname + '/public/table.html', 'utf8', (err, text) => {
-        res.send(text);
-    });
+    // fs.readFile(__dirname + '/public/table.html', 'utf8', (err, text) => {
+    //     res.send(text);
+    // });
+    res.render('table');
 });
  
+app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
